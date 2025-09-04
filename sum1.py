5f564ed5d6f8311d49c836cb0d96901c534902d3
Binod Paudel <binodpaudel1987@gmail.com>
2025-09-04 18:18:08 +0545

    practice of sum

---
+++ sum1.py
@@ -0,0 +1,10 @@
+num1=int(input("first number:"))
+num2=int(input("second number:"))
+sum_result=num1+num2
+diff_result=num1-num2
+prod_result=num1*num2
+quot_result=num1/num2
+print("Sum:", sum_result)
+print("Difference:", diff_result)
+print("Product:", prod_result)
+print("Quotient:", quot_result)
