f4b9466165692febe69c040ac592c67c9728f6d2
Binod Paudel <binodpaudel1987@gmail.com>
2025-09-12 20:58:51 +0545

    length of string practice files

---
+++ #length of string.py
@@ -0,0 +1,9 @@
+#length of string
+str1 = "motor"
+len1 = len(str1)
+str2 = "cycle"
+len2 = len(str2)
+print(len1)
+print(len2)
+final_str = str1 + str2
+print(final_str)
---
+++ final_str_with_empty.py
@@ -0,0 +1,14 @@
+#length of string
+str1 = "motor"
+len1 = len(str1)
+str2 = "cycle"
+len2 = len(str2)
+print(len1)
+print(len2)
+final_str = str1 + str2
+print(final_str)
+#print length of final_str
+print(len(final_str))
+#length of final_str with empty space
+final_str = str1+ " " + str2
+print(len(final_str))
